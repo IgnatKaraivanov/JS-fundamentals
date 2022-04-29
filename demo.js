b25db3a8487864b930050107e8651c24b574ec41
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-04-29 07:40:41 +0300

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,19 @@
+function addAndSubstract(input){
+    let arrOfNumbers = input;
+    let modifyNumbersArr =[];
+    let firstArrSum =0;
+    let modifyArrSum =0;
+    let arrLength =arrOfNumbers.length;
+
+    for (let index = 0; index < arrLength; index++) {
+        console.log(arrOfNumbers[index]);
+        if(index %2 === 0){
+            let newNumber =cu
+
+        }
+        
+    }
+
+   
+}
+addAndSubstract([5, 15, 23, 56, 35]);
