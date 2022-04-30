84297973d9d1ff691985e30924055e759b51534a
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-04-30 08:58:57 +0300

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,11 @@
+function primeNumChecker (number){
+    if(number /number == 1 && number/1 == number){
+        console.log(true)
+    }else{
+        console.log(false)
+    }
+}
+primeNumChecker(7);
+primeNumChecker(8);
+primeNumChecker(81);
+primeNumChecker(92);
