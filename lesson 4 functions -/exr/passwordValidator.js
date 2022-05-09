db4068f7fd374d33f889469d524ad869985a4a23
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-05-09 06:12:43 +0300

    Add files via upload

---
+++ lesson 4 functions -/exr/passwordValidator.js
@@ -0,0 +1,16 @@
+function passwordValidator(input) {
+  let passwordLength = input.length();
+  console.log(passwordLength);
+    // if (passwordLength >= 6 && passwordLength <= 10) {
+    //   return "Password is valid";
+    // } else {
+    //   return "Password must be between 6 and 10 characters";
+    // }
+    
+  }
+  
+
+
+passwordValidator("logIn");
+// passwordValidator('MyPass123');
+// passwordValidator('Pa$s$s');
---
+++ lesson 4 functions -/exr/smallestOfThreeNumbers.js
@@ -0,0 +1,6 @@
+function smallestNum(num1,num2,num3){
+    let smalestNumber =Math.min(num1,num2,num3);
+    console.log(smalestNumber);
+
+}
+smallestNum(2,5,3)
