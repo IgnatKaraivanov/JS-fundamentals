2846d2b081a74617fdbe3134675ecba230aaac48
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-05-31 06:32:12 +0300

    Add files via upload

---
+++ lesson 6 Objects and Classes/02.city.js
@@ -0,0 +1,23 @@
+function cityInfo(name, area, population, country, postcode) {
+    let props =Object.keys(cityInfo);
+    for (prop of  props ) {
+        console.log(``)
+        
+    }
+
+}
+  console.log(cityInfo({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000",
+  }));
+  consloe.log(cityInfo({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000",
+  }));
+
---
+++ lesson 6 Objects and Classes/1.Person Info.js
@@ -0,0 +1,17 @@
+function personInfo(name, area, population,country,postcode) {
+  let result = {
+    name:name,
+    area: area,
+    population: population,
+    country: country,
+    postcode: postcode
+  };
+  return result;
+}
+console.log({
+  name: "Sofia",
+  area: 492,
+  population: 1238438,
+  country: "Bulgaria",
+  postCode: "1000",
+});
