dd1a33acceac373d12f26ac1a858710711ee9914
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-06-03 06:53:44 +0300

    Add files via upload

---
+++ lesson 6 Objects and Classes/City.js
@@ -0,0 +1,26 @@
+function cityPrinter(city){
+    let cityEntries = Object.entries(city)
+    for (let [cityKey, cityValue] of cityEntries) {
+        console.log(`${cityKey} -> ${cityValue}`);
+    }
+    
+
+};
+
+cityPrinter({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+);
+console.log('-------------------');
+cityPrinter({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+}
+);
