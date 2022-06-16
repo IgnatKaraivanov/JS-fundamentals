2acc99125db49f9a2f97796d5eb655df2f0001c4
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-06-17 00:06:34 +0300

    Add files via upload

---
+++ lesson 8 Text Processing/1.Print Characters.js
@@ -0,0 +1,13 @@
+function solve(string){
+    // for (let index = 0; index < string.length; index++) {
+    //     console.log(string[index]);
+        
+    // }
+    // for (let char of string) {
+    //     console.log(char);
+        
+    // }
+    console.log(string.split().join('\n'));
+}
+
+solve('AWord ddd');
---
+++ lesson 8 Text Processing/2.Substring.js
@@ -0,0 +1,10 @@
+function solve(string,start,end){
+    let first=start
+    let second = end
+    console.log(string.substring(first,second+first));
+
+}
+
+solve('ASentence', 1, 8);
+
+solve('SkipWord', 4, 7);
