01a9a476517595fdeedf4e649504aad647916e63
IgnatKaraivanov <103810206+IgnatKaraivanov@users.noreply.github.com>
2022-06-28 05:49:36 +0300

    Add files via upload

---
+++ lesson 10 prep exam/HOW TO BREAK THE TASK 01.js
@@ -0,0 +1,19 @@
+// take text from input 
+// parse commands 
+// Insert Space 
+// -teake left and right part (divide at index)
+// -text= left +space+right
+// -print result 
+// return left +space+right
+
+// Reverse 
+// -find location of substring 
+// -if found:
+// -take left and right with slice
+// -return left+right+reversed substring
+// -otherwise print eror
+
+// ChangeAll 
+// -replace all occurrences of substring with replacment 
+// -print current text
+// print result
